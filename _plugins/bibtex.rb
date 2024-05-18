09210c98ee7f1df3506149202be1a319eb1febe7
EirikTengesdal <82897707+EirikTengesdal@users.noreply.github.com>
2024-05-18 14:55:30 +0000

    Deploying to gh-pages from @ EirikTengesdal/EirikTengesdal.github.io@e84f0c368877d11f94fa445db620b57d5c30e371 🚀

---
+++ _plugins/bibtex.rb
@@ -0,0 +1,14 @@
+require "bibtex"
+require "pandoc-ruby"
+
+class Pandoc < BibTeX::Filter
+  def apply(value)
+    value = value.to_s
+    return value unless %w[\ { } $].any? { |c| value.include?(c) }
+    html = PandocRuby.convert(value, :mathjax, from: :latex, to: :html)
+    html.gsub!(%r{</?p>}, "") if html.scan("<p>").size == 1
+    html.strip
+  end
+end
+
+# https://github.com/inukshuk/jekyll-scholar/issues/2#issuecomment-447238354
